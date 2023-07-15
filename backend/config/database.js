3cb28880cec9816e64e605f559b2e15e56e64a48
christine <christine@umidigital.co.uk>
2023-07-15 19:08:05 +0100

    Changed from postgres back to sql

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,21 +1,12 @@
-// const path = require('path');
-
-// module.exports = ({ env }) => ({
-//   connection: {
-//     client: 'sqlite',
-//     connection: {
-//       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-//     },
-//     useNullAsDefault: true,
-//   },
-// });
+const path = require('path');
 
 module.exports = ({ env }) => ({
   connection: {
-    client: "postgres",
+    client: 'sqlite',
     connection: {
+      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
       host: env("DATABASE_HOST", "127.0.0.1"),
-      port: env.int("DATABASE_PORT", 5432),
+      port: env.int("DATABASE_PORT", 3306),
       database: env("DATABASE_NAME", "strapi"),
       user: env("DATABASE_USERNAME", ""),
       password: env("DATABASE_PASSWORD", ""),
@@ -23,3 +14,18 @@ module.exports = ({ env }) => ({
     useNullAsDefault: true,
   },
 });
+
+
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: "postgres",
+//     connection: {
+//       host: env("DATABASE_HOST", "127.0.0.1"),
+//       port: env.int("DATABASE_PORT", 5432),
+//       database: env("DATABASE_NAME", "strapi-recipes"),
+//       user: env("DATABASE_USERNAME", ""),
+//       password: env("DATABASE_PASSWORD", ""),
+//     },
+//     useNullAsDefault: true,
+//   },
+// });
